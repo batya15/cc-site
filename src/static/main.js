634aa93f6ce9bbfff7b481f805eddca28c1617c8
Batyr <bcharyev@andiks.com>
2015-05-20 19:15:19 +0300

    refactoring - styles

--- src/static/main.js
+++ src/static/main.js
@@ -2,13 +2,13 @@ define('main', ['jquery'], function($) {
   //  var app = new App();
   //  app.render();
 
-    $(window).scroll(function() {
+    /*$(window).scroll(function() {
         if ($(window).scrollTop() > 0) {
             $('.header').addClass('scroll');
         } else {
             $('.header').removeClass('scroll');
         }
         console.log($(window).scrollTop());
-    });
+    });*/
 
 });
